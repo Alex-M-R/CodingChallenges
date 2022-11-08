9530177771b25ba516bca024e5f0fbda895e8623
AlexMR-Unity <chaosborne123@gmail.com>
2022-11-08 13:53:07 -0600

    - Reduced lines. - Removed no recursion version - Removed comment about challenge requirement

--- src/Week_11072022/Prompt1/Harshad.java
+++ src/Week_11072022/Prompt1/Harshad.java
@@ -4,25 +4,11 @@ public class Harshad
 {
     public static boolean isHarshad(int num)
     {
-        int sum = addDigitsRecursive(num);
-        return num % sum == 0;
+        return num % addDigitsRecursive(num) == 0;
     }
 
-    // not sure if this counts as meeting the "use recursion requirement"
     private static int addDigitsRecursive(int num)
     {
-        if (num == 0)
-            return 0;
-        return (num % 10 + addDigitsRecursive(num / 10));
-    }
-
-    public static boolean isHarshadNoRecursion(int num)
-    {
-        int sum = 0;
-
-        for (int t = num; t > 0; t /= 10)
-            sum += t % 10;
-
-        return num % sum == 0;
+        return num == 0? 0 : (num % 10 + addDigitsRecursive(num / 10));
     }
 }
